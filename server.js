2672b882096cb5bbd9fc278d81b5c3710e735681
Jonathan Gomez <jongomezdev@gmail.com>
2021-04-11 13:54:48 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const path = require('path');
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Define middleware here
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ extended: false }));
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+}
+
+// Define API routes here
+
+// Send every other request to the React app
+// Define any API routes before this runs
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './client/build/index.html'));
+});
+
+// Connect to the Mongo DB
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
+
+app.listen(PORT, () => {
+  console.log(`🌎 ==> API server now on port ${PORT}!`);
+});
