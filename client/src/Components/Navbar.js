4f966dde06a4ebcd9dc0f4518cb2f56d743b7a26
Jonathan Gomez <jongomezdev@gmail.com>
2021-04-11 18:48:19 -0400

    create navbar

--- client/src/Components/Navbar.js
+++ client/src/Components/Navbar.js
@@ -1,13 +1,50 @@
 import React, { Component } from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import InputBase from '@material-ui/core/InputBase';
+import SearchIcon from '@material-ui/icons/Search';
+import Switch from '@material-ui/core/Switch';
+import { withStyles } from '@material-ui/core/styles';
+import styles from '../styles/NavbarStyles';
+import { ThemeContext } from '../contexts/ThemeContext';
 
 class Navbar extends Component {
+  static contextType = ThemeContext;
   render() {
+    const { isDarkMode, toggleTheme } = this.context;
+    const { classes } = this.props;
+
     return (
-      <div>
-        <h1>Hello World</h1>
+      <div className={classes.root}>
+        <AppBar position="static" color={isDarkMode ? 'default' : 'primary'}>
+          <Toolbar>
+            <IconButton className={classes.menuButton} color="inherit">
+              <span>F</span>
+            </IconButton>
+            <Typography className={classes.title} variant="h6" color="inherit">
+              Locale
+            </Typography>
+            <Switch onChange={toggleTheme} />
+            <div className={classes.grow} />
+            <div className={classes.search}>
+              <div className={classes.searchIcon}>
+                <SearchIcon />
+              </div>
+              <InputBase
+                placeholder="Search..."
+                classes={{
+                  root: classes.inputRoot,
+                  input: classes.inputInput,
+                }}
+              />
+            </div>
+          </Toolbar>
+        </AppBar>
       </div>
     );
   }
 }
 
-export default Navbar;
+export default withStyles(styles)(Navbar);
