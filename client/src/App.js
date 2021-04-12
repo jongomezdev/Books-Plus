7bec8d15bda4e0fa4834e73ec482b1a641db81f8
Jonathan Gomez <jongomezdev@gmail.com>
2021-04-11 20:09:05 -0400

    refactor theme context and useToggleState hook

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
-// import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Navbar from './Components/Navbar';
-// import Saved from './Components/Saved';
+import Saved from './Components/Saved';
 // import Search from './Components/Search';
 import PageContent from './Components/PageContent';
 import { ThemeProvider } from './contexts/ThemeContext';
@@ -10,13 +10,13 @@ function App() {
   return (
     <ThemeProvider>
       <PageContent>
-        {/* <BrowserRouter> */}
-        <Navbar />
-        {/* <Switch>
-            <Route path="/" component={Search} exact />
+        <BrowserRouter>
+          <Navbar />
+          <Switch>
+            {/* <Route path="/" component={Search} exact /> */}
             <Route path="/saved" component={Saved} exact />
           </Switch>
-        </BrowserRouter> */}
+        </BrowserRouter>
       </PageContent>
     </ThemeProvider>
   );
--- client/src/Components/Navbar.js
+++ client/src/Components/Navbar.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useContext } from 'react';
 import { NavLink } from 'react-router-dom';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
@@ -11,44 +11,40 @@ import { withStyles } from '@material-ui/core/styles';
 import styles from '../styles/NavbarStyles';
 import { ThemeContext } from '../contexts/ThemeContext';
 
-class Navbar extends Component {
-  static contextType = ThemeContext;
-  render() {
-    const { isDarkMode, toggleTheme } = this.context;
-    const { classes } = this.props;
-
-    return (
-      <div className={classes.root}>
-        <AppBar position="static" color={isDarkMode ? 'default' : 'primary'}>
-          <Toolbar>
-            <IconButton className={classes.menuButton} color="inherit">
-              <span role="img" aria-label="books">
-                📚
-              </span>
-            </IconButton>
-            <Typography className={classes.title} variant="h6" color="inherit">
-              Books Plus
-            </Typography>
-            <Switch onChange={toggleTheme} />
-            <div className={classes.grow} />
-            <a>Saved</a>
-            <div className={classes.search}>
-              <div className={classes.searchIcon}>
-                <SearchIcon />
-              </div>
-              <InputBase
-                placeholder="Search..."
-                classes={{
-                  root: classes.inputRoot,
-                  input: classes.inputInput,
-                }}
-              />
+function Navbar(props) {
+  const { isDarkMode, toggleTheme } = useContext(ThemeContext);
+  const { classes } = props;
+  return (
+    <div className={classes.root}>
+      <AppBar position="static" color={isDarkMode ? 'default' : 'primary'}>
+        <Toolbar>
+          <IconButton className={classes.menuButton} color="inherit">
+            <span role="img" aria-label="books">
+              📚
+            </span>
+          </IconButton>
+          <Typography className={classes.title} variant="h6" color="inherit">
+            Books Plus
+          </Typography>
+          <Switch onChange={toggleTheme} />
+          <div className={classes.grow} />
+          <NavLink to="/saved">Saved</NavLink>
+          <div className={classes.search}>
+            <div className={classes.searchIcon}>
+              <SearchIcon />
             </div>
-          </Toolbar>
-        </AppBar>
-      </div>
-    );
-  }
+            <InputBase
+              placeholder="Search..."
+              classes={{
+                root: classes.inputRoot,
+                input: classes.inputInput,
+              }}
+            />
+          </div>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
 }
 
 export default withStyles(styles)(Navbar);
