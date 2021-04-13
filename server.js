e8cda257e87f1c9bde6ace61fde4015ab852b55e
Jonathan Gomez <jongomezdev@gmail.com>
2021-04-13 18:46:17 -0400

    Change model file name

--- server.js
+++ server.js
@@ -6,8 +6,6 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-// Define middleware here
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json({ extended: false }));
 
