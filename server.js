e7719d3f10df75d15be7e6ab8199ceda11c2368c
Jonathan Gomez <jongomezdev@gmail.com>
2021-04-13 20:38:57 -0400

    testing deploy

--- server.js
+++ server.js
@@ -2,27 +2,19 @@ const express = require('express');
 const mongoose = require('mongoose');
 const routes = require('./routes');
 
-const PORT = process.env.PORT || 3001;
-
 const app = express();
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json({ extended: false }));
+const PORT = process.env.PORT || 3001;
 
-// Serve up static assets (usually on heroku)
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
 }
-// Define API routes here
+
 app.use(routes);
 
-// Send every other request to the React app
-// Define any API routes before this runs
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, './client/build/index.html'));
-});
-
-// Connect to the Mongo DB
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
